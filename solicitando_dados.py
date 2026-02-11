654e3ad7bcc43b59a9203263057e68cf2cb7cc7b
cauesilva07 <caue3859@gmail.com>
2026-02-11 16:58:24 -0300

    Add files via upload

---
+++ solicitando_dados.py
@@ -0,0 +1,14 @@
+import os
+
+os.system("cls || clear")
+
+# ENTRADA
+nome = input("Digite seu nome: ")
+idade = int(input("Digite seu nome: ") )
+altura = float(input("Digite sua altura: ") )
+ 
+# SAÍDA 
+print("Nome: ", nome)
+print("Idade: ", idade)
+print("Altura: ", altura)
+
---
+++ tipagem_estatica.py
@@ -0,0 +1,20 @@
+# TIPAGEM ESTÁTICA
+
+nome: str
+idade: int
+altura: float
+matriculado: bool
+
+#ENTRADA
+
+nome = "Marta"
+idade= 23
+altura= 1.70
+matriculado: True
+
+  #SAÍDA
+  
+print("Nome: ", nome)
+print("idade: ", idade)
+print("altura: ", altura)
+print("matriculado: ", matriculado)
